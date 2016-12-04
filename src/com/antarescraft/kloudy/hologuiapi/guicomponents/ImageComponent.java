e45f16ab429e250549dd03767627e75c3860d0d1
Kloudy <kloudy@antarescraft.com>
2016-12-04 10:46:21 -0700

    added setter methods for the ImageComponent

--- src/com/antarescraft/kloudy/hologuiapi/guicomponents/ImageComponent.java
+++ src/com/antarescraft/kloudy/hologuiapi/guicomponents/ImageComponent.java
@@ -88,14 +88,34 @@ public class ImageComponent extends GUIComponent
 		return width;
 	}
 	
+	public void setWidth(int width)
+	{
+		this.width = width;
+	}
+	
 	public int getHeight()
 	{
 		return height;
 	}
 	
+	public void setHeight(int height)
+	{
+		this.height = height;
+	}
+	
 	public String getImageName()
 	{
 		return imageName;
+	}
+	
+	public void setImageName(String imageName)
+	{
+		this.imageName = imageName;
+	}
+	
+	public void setLines(String[][] lines)
+	{
+		this.lines = lines;
 	}
 	
 	@Override
