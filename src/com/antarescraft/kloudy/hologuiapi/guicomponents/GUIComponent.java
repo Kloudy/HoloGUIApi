c7a273a4ef99b9515abbe4574c9a75757a2c6099
BuildTools <unconfigured@null.spigotmc.org>
2017-01-12 08:51:40 -0700

    -

--- src/com/antarescraft/kloudy/hologuiapi/guicomponents/GUIComponent.java
+++ src/com/antarescraft/kloudy/hologuiapi/guicomponents/GUIComponent.java
@@ -2,19 +2,15 @@ package com.antarescraft.kloudy.hologuiapi.guicomponents;
 
 import org.bukkit.entity.Player;
 
-import com.antarescraft.kloudy.hologuiapi.HoloGUIPlugin;
 import com.antarescraft.kloudy.hologuiapi.playerguicomponents.PlayerGUIComponent;
 import com.antarescraft.kloudy.plugincore.config.BooleanConfigProperty;
 import com.antarescraft.kloudy.plugincore.config.ConfigElement;
 import com.antarescraft.kloudy.plugincore.config.ConfigElementKey;
 import com.antarescraft.kloudy.plugincore.config.ConfigProperty;
-import com.antarescraft.kloudy.plugincore.config.DoubleConfigProperty;
 import com.antarescraft.kloudy.plugincore.config.OptionalConfigProperty;
 
 public abstract class GUIComponent
-{
-	protected HoloGUIPlugin holoGUIPlugin;
-	
+{	
 	@ConfigElementKey
 	protected String id;
 	
@@ -26,7 +22,6 @@ public abstract class GUIComponent
 	@ConfigProperty(key = "label")
 	protected String label;
 	
-	//TODO: Figure out what to do with this as the default value is different based on type
 	@OptionalConfigProperty
 	@ConfigProperty(key = "label-distance")
 	protected double labelDistance;
@@ -41,33 +36,11 @@ public abstract class GUIComponent
 	@ConfigProperty(key = "hidden")
 	protected boolean hidden;
 	
-	public GUIComponent(GUIComponentProperties properties)
-	{
-		this.holoGUIPlugin = properties.getHoloGUIPlugin();
-		this.id = properties.getId();
-		this.position = properties.getPosition();
-		this.label = properties.getLabel();
-		this.labelDistance = properties.getLabelDistance();
-		this.alwaysShowLabel = properties.alwaysShowLabel();
-		this.hidden = properties.isHidden();
-	}
-	
 	public abstract PlayerGUIComponent initPlayerGUIComponent(Player player);
 	public abstract void updateIncrement();//updates the guicomponent's next incremental state
 	public abstract String[] updateComponentLines(Player player);
 	public abstract double getDisplayDistance();
 	public abstract double getLineHeight();
-	public abstract GUIComponent clone();
-	
-	public GUIComponentProperties cloneProperties()
-	{
-		return new GUIComponentProperties(holoGUIPlugin, id, pageId, position, label, labelDistance, alwaysShowLabel, hidden);
-	}
-	
-	public HoloGUIPlugin getHoloGUIPlugin()
-	{
-		return holoGUIPlugin;
-	}
 	
 	public String getId()
 	{
