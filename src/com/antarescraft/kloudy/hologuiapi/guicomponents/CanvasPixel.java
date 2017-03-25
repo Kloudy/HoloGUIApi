867af792fb070ebb0409eddb633d95e1ccb8ceae
Kloudy <kloudy@antarescraft.com>
2017-03-25 12:41:12 -0600

    -

--- src/com/antarescraft/kloudy/hologuiapi/guicomponents/CanvasPixel.java
+++ src/com/antarescraft/kloudy/hologuiapi/guicomponents/CanvasPixel.java
@@ -3,6 +3,7 @@ package com.antarescraft.kloudy.hologuiapi.guicomponents;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import com.antarescraft.kloudy.hologuiapi.HoloGUIApi;
 import com.antarescraft.kloudy.hologuiapi.imageprocessing.MinecraftColor;
@@ -124,5 +125,59 @@ public class CanvasPixel
 	public void setDistance(double distance)
 	{
 		this.distanceOffset = distance;
+	}
+	
+	private Location calculateArmorStandLocation(int rowIndex, Location origin, Vector vect, double distance, double lineHeight, 
+			double verticalOffset, double horizontalOffset)
+	{			
+		double xi = origin.getX();
+		double yi = origin.getY();
+		double zi = origin.getZ();
+		
+		//rotate vect by horizontal offset
+		double horizontalRadianAngle = horizontalOffset * Math.PI/3.72;
+		double verticalRadianAngle = verticalOffset * Math.PI/4.7;
+
+		vect = rotateAboutYAxis(vect, horizontalRadianAngle);
+		//double horizontalDistance = (distance * Math.sin(horizontalRadianAngle)) / Math.sin((Math.PI/2) - horizontalOffset);
+		//Vector orthogVector = new Vector(vect.getZ(), 0, -vect.getX());
+		
+		//doing rotation relative origin <0,0,0> will add xi,yi,zi in at the end
+		double x0 = (vect.getX() * distance);
+		double y0 = (vect.getY() * distance);
+		double z0 = (vect.getZ() * distance);
+		//cartesian conversion to spherical coordinates of where player is looking 'distance' blocks away
+		double r0 = Math.sqrt((x0*x0)+(y0*y0)+(z0*z0));
+		double theta0 = Math.acos((y0 / r0));
+		double phi0 = Math.atan2(z0,  x0);
+		
+		//rowIndex 0 is the label, rotate the label up a line length so it sits with a one row gap above the component
+		if(rowIndex == 0) lineHeight *= 2;
+		
+		//rotate theta0 by deltaTheta
+		double r1 = r0;
+		double theta1 = theta0 + (rowIndex * lineHeight) - verticalRadianAngle;
+		double phi1 = phi0;
+		
+		//convert spherical coordinates back into cartesian coordinates
+		double y1 = r1 * Math.cos(theta1);
+		double x1 = r1 * Math.cos(phi1) * Math.sin(theta1);
+		double z1 = r1 * Math.sin(theta1) * Math.sin(phi1);
+		x1 += xi;
+		y1 += yi;
+		z1 += zi;
+		
+		//x1 += orthogVector.getX() * horizontalDistance;
+		//z1 += orthogVector.getZ() * horizontalDistance;
+		return new Location(origin.getWorld(), x1, y1,  z1);
+	}
+	
+	protected Vector rotateAboutYAxis(Vector vector, double radians)
+	{
+		double x = (vector.getZ() * Math.sin(radians)) + (vector.getX() * Math.cos(radians));
+		double y = vector.getY();
+		double z = (vector.getZ() * Math.cos(radians) ) - (vector.getX() * Math.sin(radians));
+	
+		return new Vector(x, y, z);
 	}
 }
--- src/com/antarescraft/kloudy/hologuiapi/playerguicomponents/PlayerGUICanvasComponent.java
+++ src/com/antarescraft/kloudy/hologuiapi/playerguicomponents/PlayerGUICanvasComponent.java
@@ -17,6 +17,15 @@ public class PlayerGUICanvasComponent extends PlayerGUIComponent
 		super(player, canvasComponent);
 		
 		pixels = new CanvasPixel[canvasComponent.getProperties().getHeight()][canvasComponent.getProperties().getWidth()];
+		
+		// Initialize all of the CanvasPixels.
+		for(int i = 0; i < canvasComponent.getProperties().getHeight(); i++)
+		{
+			for(int j = 0; j < canvasComponent.getProperties().getWidth(); j++)
+			{
+				pixels[i][j] = new CanvasPixel(j, i);
+			}
+		}
 	}
 	
 	public CanvasComponent getCanvasComponent()
@@ -35,8 +44,9 @@ public class PlayerGUICanvasComponent extends PlayerGUIComponent
 		for(int i = 0; i < canvas.getProperties().getHeight(); i++)
 		{
 			for(int j = 0; j < canvas.getProperties().getWidth(); j++)
-			{			
-				pixels[i][j] = new CanvasPixel(PacketManager.);
+			{
+				pixels[i][j].setColor(color);
+				pixels[i][j].update(player);
 			}
 		}
 	}
@@ -58,51 +68,5 @@ public class PlayerGUICanvasComponent extends PlayerGUIComponent
 	public void updateLocation(Location lookLocation, boolean stationary) 
 	{
 		
-	}
-	
-	@Override
-	protected Location calculateArmorStandLocation(int rowIndex, Location origin, Vector vect, double distance, double lineHeight, 
-			double verticalOffset, double horizontalOffset)
-	{			
-		double xi = origin.getX();
-		double yi = origin.getY();
-		double zi = origin.getZ();
-		
-		//rotate vect by horizontal offset
-		double horizontalRadianAngle = horizontalOffset * Math.PI/3.72;
-		double verticalRadianAngle = verticalOffset * Math.PI/4.7;
-
-		vect = rotateAboutYAxis(vect, horizontalRadianAngle);
-		//double horizontalDistance = (distance * Math.sin(horizontalRadianAngle)) / Math.sin((Math.PI/2) - horizontalOffset);
-		//Vector orthogVector = new Vector(vect.getZ(), 0, -vect.getX());
-		
-		//doing rotation relative origin <0,0,0> will add xi,yi,zi in at the end
-		double x0 = (vect.getX() * distance);
-		double y0 = (vect.getY() * distance);
-		double z0 = (vect.getZ() * distance);
-		//cartesian conversion to spherical coordinates of where player is looking 'distance' blocks away
-		double r0 = Math.sqrt((x0*x0)+(y0*y0)+(z0*z0));
-		double theta0 = Math.acos((y0 / r0));
-		double phi0 = Math.atan2(z0,  x0);
-		
-		//rowIndex 0 is the label, rotate the label up a line length so it sits with a one row gap above the component
-		if(rowIndex == 0) lineHeight *= 2;
-		
-		//rotate theta0 by deltaTheta
-		double r1 = r0;
-		double theta1 = theta0 + (rowIndex * lineHeight) - verticalRadianAngle;
-		double phi1 = phi0;
-		
-		//convert spherical coordinates back into cartesian coordinates
-		double y1 = r1 * Math.cos(theta1);
-		double x1 = r1 * Math.cos(phi1) * Math.sin(theta1);
-		double z1 = r1 * Math.sin(theta1) * Math.sin(phi1);
-		x1 += xi;
-		y1 += yi;
-		z1 += zi;
-		
-		//x1 += orthogVector.getX() * horizontalDistance;
-		//z1 += orthogVector.getZ() * horizontalDistance;
-		return new Location(origin.getWorld(), x1, y1,  z1);
 	}
 }
