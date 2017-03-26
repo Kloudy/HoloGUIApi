177e69edcb421e48aef18ea0cfca2bc12fdc3ebc
Kloudy <kloudy@antarescraft.com>
2017-03-25 20:02:39 -0600

    improved canvas pixel positioning

--- src/com/antarescraft/kloudy/hologuiapi/playerguicomponents/PlayerGUICanvasComponent.java
+++ src/com/antarescraft/kloudy/hologuiapi/playerguicomponents/PlayerGUICanvasComponent.java
@@ -117,10 +117,10 @@ public class PlayerGUICanvasComponent extends PlayerGUIComponent
 		{
 			for(int j = 0; j < getCanvasComponent().getProperties().getWidth(); j++)
 			{
-				pixels[i][j].render(player, calculateArmorStandLocation(i, player.getLocation(), vect, 
+				pixels[i][j].render(player, calculatePixelLocation(i, j, player.getLocation(), vect, 
 						guiComponent.getProperties().getDistance(), guiComponent.getLineHeight(), 
 						guiComponent.getProperties().getPosition().getY(), 
-						guiComponent.getProperties().getPosition().getX() - (j * guiComponent.getLineHeight() * 1.2)));
+						guiComponent.getProperties().getPosition().getX()));// - (j * guiComponent.getLineHeight() * 1.2)));
 			}
 		}
 	}
@@ -135,12 +135,57 @@ public class PlayerGUICanvasComponent extends PlayerGUIComponent
 		{
 			for(int j = 0; j < getCanvasComponent().getProperties().getWidth(); j++)
 			{
-				pixels[i][j].updateLocation(player, calculateArmorStandLocation(i, player.getLocation(), vect, 
+				pixels[i][j].updateLocation(player, calculatePixelLocation(i, j, player.getLocation(), vect, 
 						guiComponent.getProperties().getDistance(), guiComponent.getLineHeight(), 
 						guiComponent.getProperties().getPosition().getY(), 
-						guiComponent.getProperties().getPosition().getX() - (j * guiComponent.getLineHeight() * 1.2)));
+						guiComponent.getProperties().getPosition().getX()));// - (j * guiComponent.getLineHeight() * 1.2)));
 			}
 		}
+	}
+	
+	private Location calculatePixelLocation(int rowIndex, int colIndex, Location origin, Vector vect, double distance, double lineHeight, 
+			double verticalOffset, double horizontalOffset)
+	{			
+		double xi = origin.getX();
+		double yi = origin.getY();
+		double zi = origin.getZ();
+		
+		//rotate vect by horizontal offset
+		double horizontalRadianAngle = horizontalOffset * Math.PI/3.72;
+		double verticalRadianAngle = verticalOffset * Math.PI/4.7;
+
+		Vector rotatedVect = rotateAboutYAxis(vect, horizontalRadianAngle);
+		double horizontalDistance = (distance * Math.sin(horizontalRadianAngle)) / Math.sin((Math.PI/2) - horizontalOffset);
+		Vector orthogVector = new Vector(vect.getZ(), 0, -vect.getX());
+		
+		//doing rotation relative origin <0,0,0> will add xi,yi,zi in at the end
+		double x0 = (rotatedVect.getX() * distance);
+		double y0 = (rotatedVect.getY() * distance);
+		double z0 = (rotatedVect.getZ() * distance);
+		//cartesian conversion to spherical coordinates of where player is looking 'distance' blocks away
+		double r0 = Math.sqrt((x0*x0)+(y0*y0)+(z0*z0));
+		double theta0 = Math.acos((y0 / r0));
+		double phi0 = Math.atan2(z0,  x0);
+		
+		//rowIndex 0 is the label, rotate the label up a line length so it sits with a one row gap above the component
+		if(rowIndex == 0) lineHeight *= 2;
+		
+		//rotate theta0 by deltaTheta
+		double r1 = r0;
+		double theta1 = theta0 + (rowIndex * lineHeight) - verticalRadianAngle;
+		double phi1 = phi0;
+		
+		//convert spherical coordinates back into cartesian coordinates
+		double y1 = r1 * Math.cos(theta1);
+		double x1 = r1 * Math.cos(phi1) * Math.sin(theta1);
+		double z1 = r1 * Math.sin(theta1) * Math.sin(phi1);
+		x1 += xi;
+		y1 += yi;
+		z1 += zi;
+		
+		x1 += orthogVector.getX() * (horizontalDistance - (colIndex * 0.15));
+		z1 += orthogVector.getZ() * (horizontalDistance - (colIndex * 0.15));
+		return new Location(origin.getWorld(), x1, y1,  z1);
 	}
 	
 	/*
